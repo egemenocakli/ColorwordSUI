d72bb8b68c31b22b0e02e53bf78269526bbb1cfe
Egemen Ocaklı <egemenocakli@gmail.com>
2024-10-17 00:55:16 +0300

    Initial Commit

---
+++ ColorwordSUI/ColorwordSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorwordSUIApp.swift
+//  ColorwordSUI
+//
+//  Created by Emre Ocaklı on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorwordSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
