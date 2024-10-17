60a3e2d712d00ed9875e05fdf8abd1a6feba7838
Egemen Ocaklı <egemenocakli@gmail.com>
2024-10-17 03:17:57 +0300

    ui design changed

--- ColorwordSUI/Login/Screen/LoginScreen.swift
+++ ColorwordSUI/Login/Screen/LoginScreen.swift
@@ -7,70 +7,99 @@
 
 import SwiftUI
 
+
 struct LoginScreen: View {
     
     @State var email: String = ""
     @State var password: String = ""
-    
+
     var body: some View {
         
         
         GeometryReader { geometry in
-            VStack {
-                Text("Colorword").frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center).font(.system(size: 60))
-                    .frame(height: geometry.size.height * 0.3)
-              
-                GeometryReader { geometry in
-                    VStack {
-                        
-                        
-                        TextField("Email", text: $email)
-                            .font(.callout)
-                            .padding()
-                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2))
-                            .padding(.all, 10)
-                        
-                        
-                        TextField("Password", text: $password)
-                            .font(.callout)
-                            .padding()
-                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2))
-                            .padding(.all, 10)
-                            
-                        
-                        
-                        Button(action: loginButton) {
-                            Text("Login")
-                                .foregroundStyle(.white)
-                                .frame(width: 100, height: 50)
-                                .background(Color.green.opacity(0.90))
-                                .clipShape(RoundedRectangle(cornerRadius: 12))
-                        }
-                        .contentShape(Rectangle())
-                        
-                        Button(action: loginButton) {
-                            Text("Sign Up")
-                                .foregroundStyle(.white)
-                                .frame(width: 100, height: 50)
-                                .background(Color.blue.opacity(0.80))
-                                .clipShape(RoundedRectangle(cornerRadius: 12))
-                        }
-                        .contentShape(Rectangle())
-                        .padding(.top, 10)
-                        
-                        
-                        
-                        
-                        
-                    }
-                    .padding(.horizontal, 20)
-                    .frame(height: geometry.size.height * 0.6)
-                }
+            ZStack {
+                // Background gradient
+                            LinearGradient(gradient: Gradient(colors: [
+                                Color(red: 0.0, green: 0.1, blue: 0.2), // Alttaki koyu mavi
+                                Color.black // Üstte siyah
+                            ]), startPoint: .bottom, endPoint: .top)
+                            .edgesIgnoringSafeArea(.all)
                 
+                VStack {
+                    Text("Colorword").frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center).font(.system(size: 60))
+                        .frame(height: geometry.size.height * 0.3)
+                        .foregroundStyle(.white)
+                    
+                    GeometryReader { geometry in
+                        VStack {
+                            
+                            
+                            VStack {
+                                 ZStack(alignment: .leading) {
+                                     if email.isEmpty {
+                                         Text("Email")
+                                             .foregroundColor(.gray)  // Placeholder rengini burada ayarlıyoruz
+                                             .padding(.leading, 40)
+                                     }
+                                     TextField("", text: $email)
+                                         .padding()
+                                         .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray.opacity(0.70), lineWidth: 2))
+                                         .padding(.all, 10)
+                                         .foregroundColor(.white)  // Asıl yazı rengi
+                                 }
+                             }
+                            
+                            VStack {
+                                 ZStack(alignment: .leading) {
+                                     if password.isEmpty {
+                                         Text("Password")
+                                             .foregroundColor(.gray)  // Placeholder rengini burada ayarlıyoruz
+                                             .padding(.leading, 40)
+                                     }
+                                     TextField("", text: $password)
+                                         .padding()
+                                         .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray.opacity(0.70), lineWidth: 2))
+                                         .padding(.all, 10)
+                                         .foregroundColor(.white)  // Asıl yazı rengi
+                                 }
+                             }
+                         
+                            
+                            
+                            Button(action: loginButton) {
+                                Text("Login")
+                                    .foregroundStyle(.white)
+                                    .frame(width: 100, height: 50)
+                                    .background(Color(red: 100/255, green: 13/255, blue: 95/255))
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }
+                            .contentShape(Rectangle())
+                            
+                            Button(action: loginButton) {
+                                Text("Sign Up")
+                                    .foregroundStyle(.white)
+                                    .frame(width: 100, height: 50)
+                                    .background(Color.gray)
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }
+                            .contentShape(Rectangle())
+                            .padding(.top, 10)
+                            
+                            
+                            
+                            
+                            
+                        }
+                        .padding(.horizontal, 20)
+                        .frame(height: geometry.size.height * 0.6)
+                    }
+                    
+                }
             }
         }
         
     }
+
     
     func loginButton() {
         
